f73b6402954fb5de48db512ba1b7b7a2d6e571c3
Xurumbambo <christianduarte33@gmail.com>
2022-04-27 12:57:17 +0100

    done main

--- program.py
+++ program.py
@@ -2,8 +2,8 @@ import sys
 from views import view
 sys.stdout.reconfigure(encoding="utf-8")
 
-# Como solicitado este programa irá permitir que consultores de uma imobiliária possam registar
-# e gerir os imóveis que têm para venda.
+# Como solicitado este programa irá permitir que o utilizador possa armazenar e consultar nome de países.
+# O programa utiliza listas ligadas para esse propósito.
 
 if __name__ == "__main__":  #Execução do programa
     view.main()
